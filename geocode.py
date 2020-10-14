d9b0a437f3593ed1aaace734336512322e205f4d
Spencer Alves <impiaaa@gmail.com>
2020-10-13 22:53:08 -0700

    Improve geocoder results

--- geocode.py
+++ geocode.py
@@ -1,13 +1,61 @@
 from urllib.request import urlopen
 from urllib.parse import quote
-import json
+import json, re
+
+unitTypes = [re.compile(r'(\W)('+part+r')(\W+.*)', flags=re.IGNORECASE) 
+             for line in open("unit_types_numbered.txt")
+             for part in line.rstrip().split('|')]
+unitTypes.extend([re.compile(r'(\W)('+part+r')((\W|\d).*)', flags=re.IGNORECASE) 
+                  for line in open("number.txt")
+                  for part in line.rstrip().split('|')])
+unitTypes.sort(key=lambda part: len(part.pattern), reverse=True)
+
+def load(path, bounded=True):
+    url = "http://localhost:4000/v1/"+path+"&size=1"
+    if bounded: url += "&boundary.gid=whosonfirst:county:102081673"
+    #print(url)
+    result = json.load(urlopen(url))
+    assert result['type'] == "FeatureCollection"
+    # Interpolation is fine, "fallback" (i.e., street or city centroid) is not.
+    result['features'] = [f for f in result['features'] if f['properties']['match_type'] != 'fallback']
+    if len(result['features']) == 0 and bounded: return load(path, False)
+    return result
 
 def geocode(row):
-    url = "http://localhost:4000/v1/search?size=1&boundary.gid=whosonfirst:county:102081673&layers=address&text="+quote(row['address1']+', '+row['address2'])
-    result = json.load(urlopen(url))
     row = {k: v for k, v in row.items() if v}
-    assert result['type'] == "FeatureCollection"
-    result['features'] = [f for f in result['features'] if f['properties']['match_type'] != 'fallback']
+    i = row['address2'].rfind(' ')
+    zipcode = row['address2'][i+1:]
+    i = row['address2'].rfind(' ', 0, i-1)
+    city = row['address2'][:i]
+    addr = row['address1']
+    
+    # first try strict search
+    result = load("search/structured?address="+quote(addr)+"&locality="+quote(city)+"&postalcode="+quote(zipcode))
+    
+    if len(result['features']) == 0:
+        # let libpostal have a try
+        result = load("search?text="+quote(addr+", "+row['address2']))
+    
+    if len(result['features']) == 0:
+        # the most common failure case seems to be misdetected unit prefix,
+        # so try changing that out
+        for unitType in unitTypes:
+            if unitType.search(addr):
+                for replacement in "number", "suite", "unit":
+                    result = load("search?text="+quote(unitType.sub(r'\1'+replacement+r' \3', addr)+", "+row['address2']))
+                    if len(result['features']) != 0:
+                        break
+                if len(result['features']) != 0:
+                    break
+
+    if len(result['features']) == 0:
+        # now just strip the unit
+        for unitType in unitTypes:
+            if unitType.search(addr):
+                result = load("search?text="+quote(unitType.sub(r'\1', addr)+", "+row['address2']))
+                if len(result['features']) != 0:
+                    break
+
     for f in result['features']:
         f['properties'].update(row)
     return result['features']
--- main.py
+++ main.py
@@ -5,11 +5,14 @@ import json
 session, urlFormat, maxPages = parseMain("https://sdp.sccgov.org")
 output = open("socialdistance.geojsonl", 'w')
 for i in range(maxPages+1):
-    print("Scraping page", i)
+    print("Scraping page", i+1)
     link = urlFormat.format(i)
     for location in parseLocations(session, link):
         if location['address1'] != "No physical address":
-            for geocoded in geocode(location):
+            results = geocode(location)
+            if len(results) == 0:
+                print("Could not locate", location['address1']+', '+location['address2'])
+            for geocoded in results:
                 json.dump(geocoded, output)
                 output.write('\n')
     output.flush()
