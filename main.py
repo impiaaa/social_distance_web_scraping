cdc040517a008f842135ec0df64f267db4b63882
Spencer Alves <impiaaa@gmail.com>
2020-11-23 21:52:53 -0800

    Instead of overwriting an old pass, build on it and skip previously scanned pages

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from social_distance_spider import parseMain, parseLocations
 from geocode import geocodeWorker, geocodeTest
-import threading, queue
+import threading, queue, json
 from fieldnames import *
 
 NTHREADS = 1
@@ -9,7 +9,12 @@ geocodeTest()
 geocodeQueue = queue.Queue(maxsize=NTHREADS*2)
 session, urlFormat, maxPages = parseMain("https://sdp.sccgov.org")
 scannedLocations = set()
-open("socialdistance.geojsonl", 'w') # clear output file
+
+try:
+    previouslyScannedIds = {json.loads(line)['properties']['id'] for line in open("socialdistance.geojsonl")}
+except FileNotFoundError:
+    previouslyScannedIds = set()
+
 threads = [threading.Thread(target=geocodeWorker, daemon=True, args=[geocodeQueue]) for i in range(NTHREADS)]
 for thread in threads:
     thread.start()
@@ -18,8 +23,10 @@ for i in range(maxPages+1):
     print("Scraping page", i+1, "of", maxPages)
     link = urlFormat.format(i)
     for location in parseLocations(session, link):
-        if location['Address (from protocol)'] == "No physical address":
+        if location[ADDR1] == "No physical address":
             continue
+        if location['id'] in previouslyScannedIds:
+            break
         
         # Site is sorted newest first, so discard any later duplicate entries
         locTuple = (location[NAME1], location.get(NAME2, None), location[ADDR1], location[ADDR2])
@@ -28,6 +35,9 @@ for i in range(maxPages+1):
         scannedLocations.add(locTuple)
         
         geocodeQueue.put(location)
+    
+    if location['id'] in previouslyScannedIds:
+        break
     
 geocodeQueue.join()
 
